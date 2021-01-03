a91889f768ad0be4a274756d32fb5e70e647504b
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-04 01:41:45 +0530

    added menu

--- src/Menubar.tsx
+++ src/Menubar.tsx
@@ -9,14 +9,48 @@ import Student from './Student';
 import Teacher from './Teacher';
 import Logout from './Logout';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-const Menubar = () => {
-  return (
-    <Router>
-      <Route exact path="/menubar" component={Menubar} />
-      <Route path="/home" component={Home} />
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-    </Router>
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+
+
+const Menubar = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleClick = (event:any) => {
+   
+        setAnchorEl(event.currentTarget);
+      };
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
+    
+  return (
+      <div>
+    <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+    Open Menu
+  </Button>
+  <Menu
+    id="simple-menu"
+    anchorEl={anchorEl}
+    keepMounted
+    open={Boolean(anchorEl)}
+    onClose={handleClose}
+  >
+    <MenuItem onClick={handleClose}>Home</MenuItem>
+    
+    <MenuItem onClick={handleClose}>Dashboard</MenuItem>
+    <MenuItem onClick={handleClose}>Usermanagement</MenuItem>
+    <MenuItem onClick={handleClose}>Admin</MenuItem>
+    <MenuItem onClick={handleClose}>Student</MenuItem>
+    <MenuItem onClick={handleClose}>Teacher</MenuItem>
+    <MenuItem onClick={handleClose}>Logout</MenuItem>
+  </Menu>
+  </div>
   );
 }
 
