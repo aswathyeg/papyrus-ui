67f6931b39f13a325323b9afd4a6ca542bbc4a8a
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-04 01:20:09 +0530

    changed default component

--- src/Menubar.tsx
+++ src/Menubar.tsx
@@ -1,5 +1,28 @@
-import React from 'react'
-const Menubar=()=>{
-    
+
+
+import React from 'react';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import Usermanagement from './Usermanagement';
+import Admin from './Admin';
+import Student from './Student';
+import Teacher from './Teacher';
+import Logout from './Logout';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+const Menubar = () => {
+  return (
+    <Router>
+      <Route exact path="/menubar" component={Menubar} />
+      <Route path="/home" component={Home} />
+
+    </Router>
+  );
 }
+
+
+
+
+
+
 export default Menubar;
