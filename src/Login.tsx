606282a01121c52b5ba43de1ec7755e50535baf3
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-04 01:20:46 +0530

    updated route path

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Home from './Home';
+import Menubar from './Menubar';
 import LoginForm from './LoginForm';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 const Login = () => {
   return (
     <Router>
       <Route exact path="/" component={LoginForm} />
-      <Route path="/home" component={Home} />
+      <Route path="/menubar" component={Menubar} />
 
     </Router>
   );
