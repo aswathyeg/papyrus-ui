6bd8b74edf59bbe169e000e4f0c4f2fa6f7f09d0
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-03 18:05:24 +0530

    base version

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+// import Welcome from './Welcome';
+// import Clock from './Clock';
+// import Toggle from './Toggle'
+// import { render } from '@testing-library/react';
+//import ConditionalRendering from './ConditionalRendering';
+// import Calculator from './Calculator';
+// import Login from './Login';
+// import SplitPane from './SplitPane';
+// import Contacts from './Contacts';
+// import Chat from './Chat';
+ import Login from './Login';
+ //import Hooks from './Hooks';
+
+function App() {
+  
+    return(
+      <div className="App">      
+        <Login/>     
+     </div>
+     
+   );
+    
+  
+  
+  
+}
+
+
+
+
+export default App;
