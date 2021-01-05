2084137b82a0a9d2d11c88d9e558035c46cc3bdf
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-06 00:02:03 +0530

    added new component

---
+++ src/Dashboard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+class  Dashboard extends React.Component{
+    constructor(props: any) {
+        super(props);
+        this.state = {
+
+        }
+    }
+   render(){
+       return(
+           <div>
+               <p>DASHBOARD</p>
+           </div>
+       )
+   }
+}
+export default  Dashboard;
