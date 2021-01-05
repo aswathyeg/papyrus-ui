439cfd1f401339e196adba9feb22eaad29c9a353
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-06 00:18:00 +0530

    rendered sidebar components

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,23 +4,12 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './Home'
 import Dashboard from './Dashboard'
+import Usermanagement from './Usermanagement'
+import Admin from './Admin'
+import Student from './Student'
+import Teacher from './Teacher'
+import Logout from './Logout'
 import { BrowserRouter as Router} from 'react-router-dom';
-// const Home: React.FC = () => {
-//   return (
-//     <React.Fragment>
-//     <NavigationBar />
-//     <h1>Home</h1>
-//     </React.Fragment>
-//   );
-// };
-
-
-const Teams: React.FC = () => {
-  return (
-    <h1>Teams</h1>
-  );
-};
-
 const Routes = [
   {
     path: '/home',
@@ -33,9 +22,29 @@ const Routes = [
     component: Dashboard
   },
   {
-    path: '/teams',
-    sidebarName: 'Teams',
-    component: Teams
+    path: '/usermanagement',
+    sidebarName: 'Usermanagement',
+    component: Usermanagement
+  },
+  {
+    path: '/admin',
+    sidebarName: 'Admin',
+    component: Admin
+  },
+  {
+    path: '/student',
+    sidebarName: 'Student',
+    component: Student
+  },
+  {
+    path: '/teacher',
+    sidebarName: 'Teacher',
+    component: Teacher
+  },
+  {
+    path: '/logout',
+    sidebarName: 'Logout',
+    component: Logout
   },
 ];
 
