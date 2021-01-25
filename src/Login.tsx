2b70518a37e5936efeacaef022af56a8e7a559ac
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-25 17:08:33 +0530

    re-enter menubar

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,14 +1,16 @@
 import LoginForm from './LoginForm';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react';
+import Menubar from './Menubar';
 const Login = () => {
 
   
   return (
     <Router>
+      
       <Route exact path="/" component={LoginForm} />
-       {/* <Route path="/menubar" component={Menubar} />  */}
-
-    </Router>
+       <Route path="/menubar" component={Menubar} />   
+         </Router>
   );
 }
 export default Login;
