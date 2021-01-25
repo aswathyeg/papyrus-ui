9c3a9fb2397292b61dfedefa00022cb6a5e55db2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-25 17:07:09 +0530

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Menubar from './Menubar'
 
 ReactDOM.render(
-   <BrowserRouter>
-    <App />
-      <Menubar />
-   </BrowserRouter>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <div>
+            <Switch>
+                <Route
+                    exact
+                    path='/'
+                    render={(props) => <App />}
+                />
+            </Switch>
+            <Switch>
+           
+                <Route
+                    exact
+                    path='/menubar'
+                    render={(props) => <Menubar/>}
+                />
+            </Switch>
+        </div>
+    </BrowserRouter>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
