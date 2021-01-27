1e516d709877246d31368c82812fa968c6d6dac3
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-27 20:40:53 +0530

    changed declaration,rendered navbar

--- src/Admin.tsx
+++ src/Admin.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-class Admin extends React.Component{
-    constructor(props: any) {
-        super(props);
-        this.state = {
 
-        }
-    }
-   render(){
-       return(
-           <div>
-               <p> ADMIN</p>
-           </div>
-       )
-   }    
+
+import NavigationBar from './NavigationBar';
+
+
+const Admin: React.FC = (): JSX.Element => {
+  
+  return (
+    <div>
+      <NavigationBar /> 
+      <p>Admin</p>
+     
+    </div>
+  );
 }
+
 export default Admin;
