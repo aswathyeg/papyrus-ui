47664b2b6c790104daa18cf79e200975e79e9168
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-27 21:53:44 +0530

    render home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+
 import './App.css';
 import Login from './Login';
-
+import {  Route } from 'react-router-dom';
+import Home from './Home';
+import { BrowserRouter } from 'react-router-dom';
 //import Hooks from './Hooks';
 
 function App() {
@@ -9,6 +12,12 @@ function App() {
   return (
     <div className="App">
       <Login />
+  
+    <Route exact path="/home" component={Home} />
+
+    {/* <Route path="/dashboard" component={Dashboard} /> */}
+
+ 
 
     </div>
 
