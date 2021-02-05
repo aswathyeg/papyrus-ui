b0e03c1dff6d6f7a223dc5a3830788f930dd2e54
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-02-05 20:09:25 +0530

    render menubar components in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,98 @@ import React from 'react';
 
 import './App.css';
 import Login from './Login';
-import {  Route } from 'react-router-dom';
+//import {  Route } from 'react-router-dom';
 import Home from './Home';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 //import Hooks from './Hooks';
-
+import Dashboard from './Dashboard';
+import Logout from './Logout';
+import Usermanagement from './Usermanagement';
+import Admin from './Admin';
+import Student from './Student';
+import Teacher from './Teacher';
 function App() {
 
   return (
+    <BrowserRouter>
     <div className="App">
+
       <Login />
   
     <Route exact path="/home" component={Home} />
 
-    {/* <Route path="/dashboard" component={Dashboard} /> */}
+    <Switch>
 
- 
+<Route
+    exact
+    path='/dashboard'
+    render={(props) =>
+        <div>
+            <Dashboard />
+        </div>}
+/>
+</Switch>
+<Switch>
 
-    </div>
+<Route
+    exact
+    path='/usermanagement'
+    render={(props) =>
+        <div>
+            <Usermanagement />
+        </div>}
+/>
+</Switch>
+<Switch>
+
+<Route
+    exact
+    path='/admin'
+    render={(props) =>
+        <div>
+            <Admin />
+        </div>}
+/>
+</Switch>
+<Switch>
+
+<Route
+    exact
+    path='/student'
+    render={(props) =>
+        <div>
+            <Student />
+        </div>}
+/>
+</Switch>
+<Switch>
+
+<Route
+    exact
+    path='/teacher'
+    render={(props) =>
+        <div>
+            <Teacher />
+       </div>}
+/>
+</Switch>
+<Switch>
+
+<Route
+    exact
+    path='/logout'
+    render={(props) =>
+        <div>
+            <Logout />
+        </div>}
+/>
+</Switch>
+</div>
+</BrowserRouter>
+
+    
+
+  
 
   );
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,118 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Dashboard from './Dashboard';
-import Logout from './Logout';
-import Usermanagement from './Usermanagement';
-import Admin from './Admin';
-import Student from './Student';
-import Teacher from './Teacher';
+// import Dashboard from './Dashboard';
+// import Logout from './Logout';
+// import Usermanagement from './Usermanagement';
+// import Admin from './Admin';
+// import Student from './Student';
+// import Teacher from './Teacher';
 
 ReactDOM.render(
-    <BrowserRouter>
+      <BrowserRouter>
         <div>
 
             <App />
-            {/* <Switch>
-                <Route
-                    exact
-                    path='/'
-                    render={(props) => <App />}
-                />
-            </Switch>  */}
-            {/* <Switch>
-           
-                <Route
-                    exact
-                    path='/menubar'
-                    render={(props) => <Menubar/>}
-                />
-            </Switch>  */}
-
-
-            {/* <Switch>
-
-                <Route
-                    exact
-                    path='/home'
-                    render={(props) =>
-                        <div>
-                            <Home />
-                        </div>}
-                />
-            </Switch> */}
-
-            <Switch>
-
-                <Route
-                    exact
-                    path='/dashboard'
-                    render={(props) =>
-                        <div>
-                            <Dashboard />
-                        </div>}
-                />
-            </Switch>
-            <Switch>
-
-                <Route
-                    exact
-                    path='/usermanagement'
-                    render={(props) =>
-                        <div>
-                            <Usermanagement />
-                        </div>}
-                />
-            </Switch>
-            <Switch>
-
-                <Route
-                    exact
-                    path='/admin'
-                    render={(props) =>
-                        <div>
-                            <Admin />
-                        </div>}
-                />
-            </Switch>
-            <Switch>
-
-                <Route
-                    exact
-                    path='/student'
-                    render={(props) =>
-                        <div>
-                            <Student />
-                        </div>}
-                />
-            </Switch>
-            <Switch>
-
-                <Route
-                    exact
-                    path='/teacher'
-                    render={(props) =>
-                        <div>
-                            <Teacher />
-                       </div>}
-                />
-            </Switch>
-            <Switch>
-
-                <Route
-                    exact
-                    path='/logout'
-                    render={(props) =>
-                        <div>
-                            <Logout />
-                        </div>}
-                />
-            </Switch>
+            
         </div>
-    </BrowserRouter>,
+     </BrowserRouter>,
     document.getElementById('root')
 );
 
