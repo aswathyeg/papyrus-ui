f14d4b9d021ccb336fea14deb8a5047fdc09ed07
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-02-07 23:54:07 +0530

    removed code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,13 @@ import './App.css';
 //import Login from './Login';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-const Home = lazy(() => import('./Home'));
-const Dashboard = lazy(() => import('./Dashboard'));
-const Logout = lazy(() => import('./Logout'));
-const Usermanagement = lazy(() => import('./Usermanagement'));
-const Admin = lazy(() => import('./Admin'));
-const Student = lazy(() => import('./Student'));
-const Teacher = lazy(() => import('./Teacher'));
-// import Dashboard from './Dashboard';
-// import Logout from './Logout';
-// import Usermanagement from './Usermanagement';
-// import Admin from './Admin';
-// import Student from './Student';
-// import Teacher from './Teacher';
+
+import Dashboard from './Dashboard';
+import Logout from './Logout';
+import Usermanagement from './Usermanagement';
+import Admin from './Admin';
+import Student from './Student';
+import Teacher from './Teacher';
 function App() {
 
   return (
@@ -25,7 +19,7 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
      
       <Switch>
-        <Route exact path="/home" component={Home}/>
+        
         <Route path="/dashboard" component={Dashboard}/>
         <Route path="/usermanagement" component={Usermanagement}/>
         <Route path="/admin" component={Admin}/>
