49923b7fad069fed8054fb157a419861c16355a8
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-02-28 18:34:17 +0530

    updated

--- src/Menubar.tsx
+++ src/Menubar.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-
-
-
 import NavigationBar from './NavigationBar';
-
-
 const Menubar: React.FC = (): JSX.Element => {
   
   return (
     <div>
       <NavigationBar /> 
       
-      {/* <Switch>
-        {Routes.map((route: any) => (
-          <Route exact path={route.path} key={route.path}>
-            <route.component />
-          </Route>
-        ))}
-      </Switch> */}
-    </div>
+           </div>
   );
 }
 
